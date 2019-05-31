6eaa7ef71242c688039ff2d7de203ffbb9187996
timothyleong97 <timothy.leong@u.nus.edu>
2019-05-31 17:24:46 +0800

    added functionality for saving and deleting plans.

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,7 +3,7 @@
 /*       INITIALISATION         */
 /********************************/
 
-//repopulatePlansTable();
+repopulatePlansTable();
 repopulateModulesTable();
 
 
@@ -15,10 +15,11 @@ repopulateModulesTable();
     getters and setters for the localStorage elements
 */
 
+//each plan is a JSON object - {label: "plan1", value: "html"}
 function getSavedPlans() {
     if(localStorage.getItem('savedPlans') == null) {
-        localStorage.setItem('savedPlans','');
-        return "";
+        localStorage.setItem('savedPlans','[]');
+        return [];
     } else {
         return JSON.parse(localStorage.getItem('savedPlans'));
     }
@@ -26,10 +27,21 @@ function getSavedPlans() {
 }
 
 //save each plan as a JSON object - {label: "plan1", value: "html"}
-function setSavedPlans() {
-    
+function setSavedPlans(arr) {
+    localStorage.setItem('savedPlans',JSON.stringify(arr));
 }
 
+//makes the JSON object 
+function makeJSON(plan) {
+    var text = getCurrentModules();
+    return {
+        "name": plan,
+        "html": text
+    }
+}
+
+//moduleList refers to the latest view of the modules table.
+//If a plan is loaded, then the plan will change the latest view of the modules table.
 function getModuleList() {
     if(localStorage.getItem('moduleList') == null) {
         localStorage.setItem('moduleList','');
@@ -42,12 +54,32 @@ function getModuleList() {
 
 //save the html straight
 function setModuleList() {
-    localStorage.setItem('moduleList',$(".js--modTable tbody").html());
+    localStorage.setItem('moduleList', getCurrentModules());
 }
 
+function getCurrentModules() {
+    return $(".js--modTable tbody").html();
+}
 
+//get savedPlans, join the html, then set the plansTable body html
 function repopulatePlansTable() {
-    $('.js--plansTable tbody').html(getSavedPlans());
+    var arr = getSavedPlans();
+    var str = "";
+    for (var i = 0; i < arr.length; i++) {
+        var planName = arr[i].name;
+        //create a tr
+        str +=
+        "<tr>" +
+            "<th><a href=\"\" class=\"planLink\">" + planName +"</a></th>" +
+                      "<td class=\"deleteCell\">" +
+                          "<div class = 'buttonContainer'>" + 
+                              "<button type=\"button\" class=\"close deletePlan\" aria-label=\"Close\">" +
+                            "<span aria-hidden=\"true\">&times;</span></button>" +
+                          "</div>"+
+                      "</td>"+
+                "</tr>"
+    }
+    $('.js--plansTable tbody').html(str);
 }
 
 function repopulateModulesTable() {
@@ -89,7 +121,7 @@ $('a[href*="#"]')
     load the autocomplete 
 */
 
- $.getJSON("https://api.nusmods.com/v2/2018-2019/moduleList.json", function(data) {
+ $.getJSON("vendors/data/moduleList.json", function(data) {
           var list = data.map(function(i){return i.moduleCode + " - " + i.title});
           new Awesomplete(document.querySelector("#moduleSearchDiv input"),{ 
              list: list,
@@ -128,21 +160,24 @@ $("input.awesomplete").on("awesomplete-selectcomplete", function(){
 });
 
 /*
-    delete row when close button is clicked
+    delete row when close button is clicked for modules 
 */
 
-$(document).on('click','.close', function(){
+
+$(document).on('click','.deleteModule', function(){
     $(this).parent().parent().parent().remove();
+    setModuleList();
 })
 
 
 /*
-    ------------------------LOGIC FOR THE SAVE AS NEW... BUTTON---------------------------- 
+    -----------------LOGIC FOR THE SAVE / SAVE AS NEW... BUTTON---------------------------- 
 */
 
 function emptyFieldAlert() {
     $('.savePlan .no-name').show(200);
     $('.savePlan .savedSuccessfully').hide();
+     $('.savePlan .duplicate-name').hide();
     setTimeout(function(){
         $('.savePlan .no-name').hide(300);
     }, 1500);
@@ -150,7 +185,8 @@ function emptyFieldAlert() {
 
 
 function successfulSaveAlert() {
-     $('.savePlan .no-name ').hide(200);
+        $('.savePlan .duplicate-name').hide();
+        $('.savePlan .no-name ').hide();
         $('.savePlan .savedSuccessfully ').html("<strong>" + $('.savePlanBar input').val()+"</strong> saved!")
         $('.savePlan .savedSuccessfully ').show(200);
         setTimeout(function(){
@@ -162,16 +198,72 @@ function successfulSaveAlert() {
     controls for the alerts for saving NEW plan
 */
 
+function updateAndResyncPlans(name, arr) { 
+    var obj = makeJSON(name);
+    arr.push(obj);
+    setSavedPlans(arr);
+    repopulatePlansTable();
+    successfulSaveAlert();
+}
+
 $(".saveAsNewButton").on('click', function(){
     if ($('.savePlanBar input').val()==""){
         emptyFieldAlert();
     } else {
-        successfulSaveAlert();
+        var name = $('.savePlanBar input').val();
+        var arr = getSavedPlans();
+        var duplicate = false;
+        for (var i = 0; i < arr.length;i++) {
+            if (arr[i].name== name) {
+                duplicate = true;
+                break;
+            }
+        }
+        if (duplicate) {
+            //show the duplicate name div
+            $('.savePlan .no-name').hide();
+            $('.savePlan .savedSuccessfully').hide();
+            $('.savePlan .duplicate-name').show(200);
+            setTimeout(function(){
+                $('.savePlan .duplicate-name').hide(300);
+            }, 1500);
+            
+        } else {
+            updateAndResyncPlans(name,arr);
+        }
+        
+        //empty the input so that users cannot double click
+        $('.savePlanBar input').val("");
     }
 })
 
 
-//actually save the plan in localStorage
+//controls for saving existing plan
+
+$(".saveButton").on('click', function(){
+    if ($('.savePlanBar input').val()==""){
+        emptyFieldAlert();
+    } else {
+        var name = $('.savePlanBar input').val();
+        var arr = getSavedPlans();
+        var duplicate = false;
+        for (var i = 0; i < arr.length;i++) {
+            if (arr[i].name== name) {
+                duplicate = true;
+                break;
+            }
+        }
+        if (duplicate) {
+            //TODO: show the overwrite modal
+            //TODO: save in localStorage
+        } else {
+            updateAndResyncPlans(name, arr);
+        }
+        
+        //empty the input so that users cannot double click
+        $('.savePlanBar input').val("");
+    }
+});
 
 
 /*
@@ -194,4 +286,51 @@ $('#moduleSearchBar').keypress(function(event){
         event.preventDefault();
     }
 });
+
+/*
+    Loading a plan by clicking on a plan name
+*/
+
+
+$('body').on('click', '.planLink', function(){
+    var planName = $(this).text();
+    $('.savePlanBar input').val(planName);
+    var plansArr = getSavedPlans();
+    var html = "";
+    for(var i = 0; i < plansArr.length; i++) {
+        if (plansArr[i].name == planName) {
+            html = plansArr[i].html;
+            break;
+        }
+    }
+    //change the current view
+    $(".js--modTable tbody").html(html);
+    setModuleList();
+    
+})
+
+/*
+    Deleting a plan
+*/
+
+$('body').on('click', '.deletePlan', function(){
+    var me = $(this);
+    $("#deletePlanModal").modal('show');
+    $('#confirmDeletePlanButton').click(function(){
+        var planName = me.parent().parent().prev().text();
+        var plansArr = getSavedPlans();
+        for(var i = 0; i < plansArr.length; i++) {
+            if (plansArr[i].name == planName) {
+                plansArr.splice(i,1);
+                break;
+            }
+        }
+        setSavedPlans(plansArr);
+        repopulatePlansTable();
+         $("#deletePlanModal").modal('hide');
+    })
+    
+//    //TODO: convert this to modal
+    
+})
 
