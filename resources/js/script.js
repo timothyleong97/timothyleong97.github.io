8a06db05bfe918441f3277973a12690bf21990c3
timothyleong97 <timothy.leong@u.nus.edu>
2019-06-03 09:37:56 +0800

    added functionality to export table as csv

--- resources/js/script.js
+++ resources/js/script.js
@@ -439,7 +439,9 @@ $(".saveButton").on('click', function(){
 
 $('.clearAllButton').click(function(){
     $(".js--modTable tbody").html("");
+    $(".table-list table tbody").html("");
     $('#clearTableModal').modal('hide');
+    updateNumSems();
     setModuleList();
 })
 
@@ -575,3 +577,51 @@ function renumberSemesters(num) {
         num++;
     }
 }
+
+$('.exportToCSVButton').click(function(){
+ 	export_table_to_csv("table.csv");  
+})
+
+function export_table_to_csv(filename) {
+	var csv = [];
+	var rows = document.querySelectorAll(".table-list table tr");
+	
+    for (var i = 0; i < rows.length; i++) {
+		var row = [], cols = rows[i].querySelectorAll("td, th");
+		
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+		csv.push(row.join(","));		
+	}
+
+    // Download CSV
+    download_csv(csv.join("\n"), filename);
+}
+
+function download_csv(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV FILE
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // We have to create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Make sure that the link is not displayed
+    downloadLink.style.display = "none";
+
+    // Add the link to your DOM
+    document.body.appendChild(downloadLink);
+
+    // Lanzamos
+    downloadLink.click();
+}
+
